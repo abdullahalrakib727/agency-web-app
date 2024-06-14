bd892292c41f7a77b32e052c4db22176e93931d5
abdullahalrakib727 <abdullahalrakib30@gmail.com>
2024-06-14 19:59:45 +0600

    added all the paths

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
-
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import './App.css';
-import HomePage from './components/pages/HomePage/HomePage';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import "./App.css";
+import HomePage from "./components/pages/HomePage/HomePage";
+import About from "./components/pages/About/About";
+import Contact from "./components/pages/Contact/Contact";
+import Services from "./components/pages/Services/Services";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<HomePage/>} />
-        <Route path="/about" element={<About} />
-        <Route path="/" element={<HomePage/>} />
-        <Route path="/" element={<HomePage/>} />
+        <Route path="/" element={<HomePage />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/services" element={<Services />} />
       </Routes>
     </BrowserRouter>
   );
