66c8ab1f8a9c25fbaca66222648b419ea1eb16e4
abdullahalrakib727 <abdullahalrakib30@gmail.com>
2024-06-17 22:41:23 +0600

    added navbar styles

--- src/components/Navigation/Navbar.jsx
+++ src/components/Navigation/Navbar.jsx
@@ -7,13 +7,16 @@ const Navbar = () => {
     <nav className="max-w-screen overflow-x-hidden font-poppins">
       <div className="bg-gray-900 fixed sm:w-full w-screen z-20 left-0 right-0 top-0 border-b border-y-gray-400">
         {/* desktop navigation start */}
-        <div>
-          <Link to="/">
+        <div className=" flex flex-wrap items-center justify-between mx-auto sm:p-4 pb-2">
+          <Link to="/" className="flex items-center">
             <img
               src={Icon}
               alt="icon"
               className="hidden sm:block sm:w-[35px] sm:h-[35px]"
             />
+            <span className="ml-2 text-white font-semibold text-xl">
+                Levelup Solutions
+            </span>
           </Link>
         </div>
         {/* desktop navigation end*/}
