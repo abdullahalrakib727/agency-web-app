dae25f03d8b7c87b4caf279424a36c0dd17187b2
abdullahalrakib727 <abdullahalrakib30@gmail.com>
2024-06-18 22:59:32 +0600

    added login and getstarted button in navbar

--- src/components/Navigation/Navbar.jsx
+++ src/components/Navigation/Navbar.jsx
@@ -7,7 +7,7 @@ const Navbar = () => {
     <nav className="max-w-screen overflow-x-hidden font-poppins">
       <div className="bg-gray-900 fixed sm:w-full w-screen z-20 left-0 right-0 top-0 border-b border-y-gray-400">
         {/* desktop navigation start */}
-        <div className=" flex flex-wrap items-center justify-between mx-auto sm:p-4 pb-2">
+        <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto sm:p-4 pb-2">
           <Link to="/" className="flex items-center">
             <img
               src={Icon}
@@ -15,9 +15,23 @@ const Navbar = () => {
               className="hidden sm:block sm:w-[35px] sm:h-[35px]"
             />
             <span className="ml-2 text-white font-semibold text-xl">
-                Levelup Solutions
+              Levelup Solutions
             </span>
           </Link>
+          <div className="flex">
+            <button
+              type="button"
+              className="bg-blue-600 px-4 py-2 hidden sm:block text-white rounded-xl mr-2 hover:bg-blue-500"
+            >
+              Get Started
+            </button>
+            <button
+              type="button"
+              className="hidden sm:block text-white border font-medium rounded-lg ml-2 text-sm px-4 hover:bg-blue-300/10 mr-4"
+            >
+              Login
+            </button>
+          </div>
         </div>
         {/* desktop navigation end*/}
       </div>
