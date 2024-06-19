cad8a4ebbeb36d5cf2976807b7f390f974131e27
abdullahalrakib727 <abdullahalrakib30@gmail.com>
2024-06-19 23:12:03 +0600

    added state on navbar for hamburger menu

--- src/components/Navigation/Navbar.jsx
+++ src/components/Navigation/Navbar.jsx
@@ -1,11 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 import Icon from "../../images/level.png";
 import { Link } from "react-router-dom";
+import { GiHamburgerMenu } from "react-icons/gi";
 
 const Navbar = () => {
+
+const [open, setOpen] = useState(false);
+console.log(open);
+
+
   return (
     <nav className="max-w-screen overflow-x-hidden font-poppins">
-      <div className="bg-gray-900 fixed sm:w-full w-screen z-20 left-0 right-0 top-0 border-b border-y-gray-400">
+      <div className="bg-gray-900 fixed sm:w-full h-[50px] sm:h-auto w-screen z-20 left-0 right-0 top-0 border-b border-y-gray-400">
         {/* desktop navigation start */}
         <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto sm:p-4 pb-2">
           <Link to="/" className="flex items-center">
@@ -14,7 +20,7 @@ const Navbar = () => {
               alt="icon"
               className="hidden sm:block sm:w-[35px] sm:h-[35px]"
             />
-            <span className="ml-2 text-white font-semibold text-xl">
+            <span className="ml-2 mt-2 sm:mt-0 text-white font-semibold text-xl">
               Levelup Solutions
             </span>
           </Link>
@@ -30,6 +36,9 @@ const Navbar = () => {
               className="hidden sm:block text-white border font-medium rounded-lg ml-2 text-sm px-4 hover:bg-blue-300/10 mr-4"
             >
               Login
+            </button>
+            <button onClick={()=>setOpen(!open)}>
+                <GiHamburgerMenu size={30} className="text-white mt-2 mr-2 sm:hidden" />
             </button>
           </div>
         </div>
